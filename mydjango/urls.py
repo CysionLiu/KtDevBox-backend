3f19be2727dd7b1c0223c05b18c543136e8ad4f9
CysionLiu <liuxshang@163.com>
2018-12-13 17:31:32 +0800

    add index

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from mydjango import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
     path('app/', include("myapp.urls"))
 ]
